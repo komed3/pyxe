b9aa4817550eb329d88a65db69b8bd8399cde96c
komed3 <pk97@outlook.de>
2025-04-20 13:29:43 +0200

    basic structure

--- packages/core/src/root.ts
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-export * from './classes/ColorObject.js';
-export * from './classes/ColorSpace.js';
--- packages/core/src/services.ts
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-export { ColorSpace } from './classes/ColorSpace.js';
