d4cfba6bf14fcd64db7e34c7758bd6671c6a888f
komed3 <pk97@outlook.de>
2025-04-23 00:42:37 +0200

    manage core package exports

---
+++ packages/core/src/index.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+export { Color } from './Color.js';
+export { ColorSpace } from './ColorSpace.js';
+export { ColorLib } from './ColorLib.js';
+export { Module } from './Module.js';
+export { ModuleMethod } from './ModuleMethod.js';
+export { ConversionGraph } from './ConversionGraph.js';
+export { Validator } from './Validator.js';
+export { Parser } from './Parser.js';
---
+++ packages/core/src/root.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+export * from './Color.js';
+export * from './ColorSpace.js';
+export * from './Parser.js';
+export * from './Validator.js';
+export * from './Output.js';
+export * from './ColorObject.js';
+export * from './ColorLib.js';
+export * from './Module.js';
+export * from './ModuleMethod.js';
+export * from './ConversionGraph.js';
+export * from './Convert.js';
+
+export * from './registry/ColorSpaceRegistry.js';
+export * from './registry/ColorMethodRegistry.js';
+export * from './registry/ColorLibRegistry.js';
+export * from './registry/ModuleRegistry.js';
+export * from './registry/ModuleMethodRegistry.js';
+export * from './registry/ConversionGraphRegistry.js';
+
+export * from './services/Hook.js';
+export * from './services/PyxeError.js';
+export * from './services/Tracer.js';
