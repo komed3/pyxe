3068ad1da2cd4bc387d3e32742002b884ea81bf2
komed3 <pk97@outlook.de>
2025-04-23 17:34:35 +0200

    register color space conversions

--- packages/core/src/registry/ColorSpaceRegistry.ts
+++ packages/core/src/registry/ColorSpaceRegistry.ts
@@ -2,6 +2,7 @@
 
 import type { ColorSpaceName, ColorSpaceFactory } from '@pyxe/types';
 import { Registry } from './Registry.js';
+import { conversionGraphRegistry } from './ConversionGraphRegistry.js';
 
 export class ColorSpaceRegistry extends Registry<ColorSpaceName, ColorSpaceFactory> {
 
@@ -12,17 +13,27 @@ export class ColorSpaceRegistry extends Registry<ColorSpaceName, ColorSpaceFacto
 
         super._add( name, factory );
 
+        if ( factory.conversions ) {
+
+            conversionGraphRegistry.addMany( name, factory.conversions );
+
+        }
+
     }
 
     public remove (
         name: ColorSpaceName
     ) : void {
 
+        conversionGraphRegistry.removeAll( name );
+
         super._remove( name );
 
     }
 
     public clear () {
+
+        conversionGraphRegistry.clear();
 
         super._clear();
 
