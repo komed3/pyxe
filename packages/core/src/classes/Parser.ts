9a25b33fea7b6e33d40e24fb4005124f53cb2069
komed3 <pk97@outlook.de>
2025-04-27 18:05:05 +0200

    implement auto parser + min fixes

--- packages/core/src/classes/Parser.ts
+++ packages/core/src/classes/Parser.ts
@@ -4,6 +4,7 @@ import type { ColorInput, ColorInstance, ColorObjectFactory, ColorSpaceName } fr
 import { ChannelHelper } from '@pyxe/utils';
 import { ColorSpace } from './ColorSpace.js';
 import { hook } from '../services/Hook.js';
+import { handleError } from '../services/ErrorUtils.js';
 import { debug } from '../services/Debug.js';
 
 const instances: Map<ColorSpaceName, Parser> = new Map ();
@@ -54,6 +55,14 @@ export class Parser {
     public parse (
         input: ColorInput
     ) : ColorObjectFactory | false {
+
+        const early = hook.filter( 'Parser::earlyParse', undefined, input, this );
+
+        if ( early !== undefined ) {
+
+            return early;
+
+        }
 
         hook.run( 'Parser::beforeParse', input, this );
 
@@ -125,12 +134,70 @@ export class Parser {
 
     }
 
-    public static parseAuto (
+    public static parseAs (
         input: ColorInput,
+        space: ColorSpaceName,
         strict: boolean = false
     ) : ColorObjectFactory | false {
 
-        //
+        hook.run( 'Parser::parseAs', input, space, strict, this );
+
+        try {
+
+            if ( ColorSpace.has( space, false ) ) {
+
+                const parser = Parser.getInstance( space, strict );
+
+                return parser.parse( input );
+
+            }
+
+            return false;
+
+        } catch {
+
+            return false;
+
+        }
+
+    }
+
+    public static parseAuto (
+        input: ColorInput,
+        strict: boolean = false,
+        safe: boolean = true
+    ) : ColorObjectFactory | false {
+
+        input = input.toString().trim();
+
+        hook.run( 'Parser::beforeAutoParse', input, strict, safe, this );
+
+        for ( const space of [
+            ...[
+                input.split( /[\s(]+/ )[ 0 ].toLowerCase(),
+                input.substring( 0, 1 )
+            ],
+            ...ColorSpace.list()
+        ] ) {
+
+            const result = this.parseAs( input, space, strict );
+
+            if ( result !== false ) {
+
+                hook.run( 'Parser::afterAutoParse', input, strict, safe, result, this );
+
+                return result;
+
+            }
+
+        }
+
+        hook.run( 'Parser::allFailed', input, strict, safe, this );
+
+        return handleError( {
+            method: 'Parser',
+            msg: `No suitable parser found for input <${ JSON.stringify( input ) }>`
+        }, safe );
 
     }
 
