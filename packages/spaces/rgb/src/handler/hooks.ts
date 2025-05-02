79b7f3e38af0b8194259795f1d18d3cabe014f06
komed3 <pk97@outlook.de>
2025-05-02 22:44:01 +0200

    add RGB color space hook handler

---
+++ packages/spaces/rgb/src/handler/hooks.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+import { ColorObjectFactory, HookHandler } from '@pyxe/types';
+import { Basic, ChannelHelper } from '@pyxe/utils';
+
+export const hooksHandler: Record<string, HookHandler> = {
+
+    'Parser::earlyParse': ( _, input, self ) : ColorObjectFactory | undefined => {
+
+        if ( self.colorSpace.name === 'rgb' && input.startsWith( '#' ) ) {
+
+            const clean = input.replace( /^#/, '' );
+
+            if ( [ 3, 4, 6, 8 ].includes( clean.length ) ) {
+
+                const [ r, g, b, a ] = clean.length <= 4
+                    ? [ ...clean ].map( c => Basic.hexdec( c.repeat( 2 ) ) )
+                    : clean.match( /../g )!.map( Basic.hexdec );
+
+                return {
+                    space: 'rgb',
+                    value: ChannelHelper.parseInstance( { r, g, b }, self.colorSpace.getChannels() ),
+                    alpha: ChannelHelper.parseAlpha( a / 255, ! self.strict ),
+                    meta: { source: input }
+                } as ColorObjectFactory;
+
+            }
+
+        }
+
+        return undefined;
+
+    }
+
+};
--- packages/spaces/rgb/src/index.ts
+++ packages/spaces/rgb/src/index.ts
@@ -1,17 +1,19 @@
 'use strict';
 
 import { Registry } from '@pyxe/core/registry';
+import { hooksHandler } from './handler/hooks.js';
 
 Registry.ColorSpace.add( 'rgb', {
     aliases: [ 'rgba', 'hex', '#' ],
+    meta: {
+        name: 'RGB',
+        description: 'Additive red-green-blue color model using 8-bit channels'
+    },
     channels: {
         r: { type: 'numeric', min: 0, max: 255, decimals: 0, name: 'Red' },
         g: { type: 'numeric', min: 0, max: 255, decimals: 0, name: 'Green' },
         b: { type: 'numeric', min: 0, max: 255, decimals: 0, name: 'Blue' }
     },
     alpha: true,
-    meta: {
-        name: 'RGB',
-        description: 'Additive red-green-blue color model using 8-bit channels'
-    }
+    hooks: hooksHandler
 } );
