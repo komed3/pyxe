c87a8b459ea76b28f113bd6086d996bbabf29db9
komed3 <pk97@outlook.de>
2025-05-02 08:48:05 +0200

    pyxe auto loader (main package)

---
+++ packages/pyxe/src/autoLoader.ts
@@ -0,0 +1,45 @@
+'use strict';
+
+import { Services } from '@pyxe/core/services';
+
+export const registry: Record<string, string[]> = {
+    space: [ 'hex', 'rgb', 'hsl', 'hsv', 'cmyk' ],
+    library: [ 'ral' ],
+    module: [ 'basic' ]
+};
+
+export const loaded: Map<string, string[]> = new Map ();
+
+const loadPackages = async () : Promise<void> => {
+
+    for ( const [ type, packages ] of Object.entries( registry ) ) {
+
+        const loadedPackages = packages.map( async ( name ) => {
+
+            try {
+
+                await import( `@pyxe/${type}-${name}` );
+
+                Services.Debugger.log( 'AutoLoader', `package <${name}> of type <${type}> loaded` );
+
+                return name;
+
+            } catch {
+
+                /** Skip uninstalled package */
+
+            }
+
+        } );
+
+        loaded.set( type,
+            ( await Promise.all( loadedPackages ) ).filter(
+                ( pkg ) : pkg is string => pkg !== undefined
+            )
+        );
+
+    }
+
+};
+
+await loadPackages();
