4e37857d70f312278ed5d762ba9c2aaeafce3664
komed3 <pk97@outlook.de>
2025-05-03 09:36:04 +0200

    Update hooks.ts

--- packages/spaces/rgb/src/handler/hooks.ts
+++ packages/spaces/rgb/src/handler/hooks.ts
@@ -6,7 +6,7 @@ import { Basic, ChannelHelper } from '@pyxe/utils';
 
 export const hooks: Record<string, HookHandler> = {
 
-    'Parser::earlyParse': ( _, input, self ) : ColorObjectFactory | undefined => {
+    'Parser::earlyParse': ( _, input, self ) : ColorObjectFactory | undefined | false => {
 
         if ( self.colorSpace.name === 'rgb' && input.startsWith( '#' ) ) {
 
@@ -32,7 +32,7 @@ export const hooks: Record<string, HookHandler> = {
             }, {
                 method: 'Space::RGB',
                 msg: `Parsing failed for HEX string <${input}>`
-            }, self.strict ) || undefined;
+            }, self.strict );
 
         }
 
