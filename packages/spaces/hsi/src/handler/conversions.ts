7d04cae4d866b941c89abffbe373df05c37555b1
komed3 <pk97@outlook.de>
2025-05-06 11:19:54 +0200

    Update conversions.ts / code cleanup

--- packages/spaces/hsi/src/handler/conversions.ts
+++ packages/spaces/hsi/src/handler/conversions.ts
@@ -11,45 +11,17 @@ export const conversions: ConversionFactory = {
         if ( input && input.space === 'hsi' ) {
 
             const { h, s, i } = input.value as HSI;
-
             const H = h * 2 * Math.PI;
 
-            const [ r, g, b ] = ( () => {
+            const adjust = ( v: number ) => i * (
+                1 + s * Math.cos( H - v * 2 * Math.PI / 3 ) /
+                Math.cos( Math.PI / 3 - ( H - v * 2 * Math.PI / 3 ) )
+            );
 
-                const adjust = ( v: number ) => i * (
-                    1 + s * Math.cos( H - v * 2 * Math.PI / 3 ) /
-                    Math.cos( Math.PI / 3 - ( H - v * 2 * Math.PI / 3 ) )
-                );
-
-                if ( s === 0 ) {
-
-                    return [ i, i, i ];
-
-                } else if ( H < 2 * Math.PI / 3 ) {
-
-                    return [
-                        adjust( 0 ),
-                        3 * i - adjust( 0 ) - i * ( 1 - s ),
-                        i * ( 1 - s )
-                    ];
-
-                } else if ( H < 4 * Math.PI / 3 ) {
-
-                    return [
-                        i * ( 1 - s ),
-                        adjust( 1 ),
-                        3 * i - adjust( 1 ) - i * ( 1 - s )
-                    ];
-
-                }
-
-                return [
-                    3 * i - adjust( 2 ) - i * ( 1 - s ),
-                    i * ( 1 - s ),
-                    adjust( 2 )
-                ];
-
-            } )();
+            const [ r, g, b ] = s === 0 ? [ i, i, i ]
+                : H < 2 * Math.PI / 3 ? [ adjust( 0 ), 3 * i - adjust( 0 ) - i * ( 1 - s ), i * ( 1 - s ) ]
+                : H < 4 * Math.PI / 3 ? [ i * ( 1 - s ), adjust( 1 ), 3 * i - adjust( 1 ) - i * ( 1 - s ) ]
+                : [ 3 * i - adjust( 2 ) - i * ( 1 - s ), i * ( 1 - s ), adjust( 2 ) ];
 
             return {
                 space: 'rgb',
