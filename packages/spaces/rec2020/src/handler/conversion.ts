6cce4f455614cd900a8342adee8e25d401086d1d
komed3 <pk97@outlook.de>
2025-05-08 11:37:40 +0200

    add conversions from Rec.709/2020 to LRGB

--- packages/spaces/rec2020/src/handler/conversion.ts
+++ packages/spaces/rec2020/src/handler/conversion.ts
@@ -2,4 +2,32 @@
 
 import type { RGB, ColorObjectFactory, ConversionFactory } from '@pyxe/types';
 
-export const conversions: ConversionFactory = {}
+export const conversions: ConversionFactory = {
+
+    lrgb: (
+        input: ColorObjectFactory | undefined
+    ) : ColorObjectFactory | undefined => {
+
+        if ( input && input.space === 'rec709' ) {
+
+            const { r, g, b } = input.value as RGB;
+
+            const adjust = ( v: number ) : number =>
+                v < 0.08145 ? v / 4.5 : ( ( v + 0.0993 ) / 1.0993 ) ** ( 1 / 0.45 );
+
+            return {
+                space: 'lrgb',
+                value: {
+                    r: adjust( r ),
+                    g: adjust( g ),
+                    b: adjust( b )
+                },
+                alpha: input.alpha,
+                meta: input.meta ?? {}
+            } as ColorObjectFactory;
+
+        }
+
+    }
+
+};
